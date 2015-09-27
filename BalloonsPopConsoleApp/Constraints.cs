5d5f059a5ec7f818754b725a27305d9484657427
Pip3r4o <pip3r4o@gmail.com>
2015-09-27 22:17:21 +0300

    Constraints fixes

--- BalloonsPopConsoleApp/Constraints.cs
+++ BalloonsPopConsoleApp/Constraints.cs
@@ -4,17 +4,11 @@ namespace BalloonsPopConsoleApp
 {
     public class Constraints : IConstraints
     {
-        public Constraints(int minValue, int maxValue, int rows, int cols)
+        public Constraints(int rows, int cols)
         {
-            this.BalloonMinValue = minValue;
-            this.BalloonMaxValue = maxValue;
             this.BoardRows = rows;
             this.BoardColumns = cols;
         }
-
-        public int BalloonMinValue { get; private set; }
-
-        public int BalloonMaxValue { get; private set; }
 
         public int BoardRows { get; private set; }
 
