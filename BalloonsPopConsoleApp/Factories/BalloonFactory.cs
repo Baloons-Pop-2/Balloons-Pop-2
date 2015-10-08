9a40e127a4b2d22191b3e2718104778a13b07994
Hri100v <hr100h@gmail.com>
2015-10-08 12:50:00 +0300

    add doc BFactory
    Documentation about BalloonFactory.

--- BalloonsPopConsoleApp/Factories/BalloonFactory.cs
+++ BalloonsPopConsoleApp/Factories/BalloonFactory.cs
@@ -6,17 +6,28 @@
     using BalloonsPop.Traversals;
     using Models;
 
+    /// <summary>
+    /// BalloonFactory pattern that take care of create instances of the Balloon class.
+    /// </summary>
     public class BalloonFactory : IBalloonFactory
     {
         private readonly Dictionary<int, IBalloon> balloonsList;
         private readonly IEffectFactory effectFactory;
 
+        /// <summary>
+        /// BalloonFactory object constructor.
+        /// </summary>
         public BalloonFactory()
         {
             this.balloonsList = new Dictionary<int, IBalloon>();
             this.effectFactory = new EffectFactory();
         }
 
+        /// <summary>
+        /// Method that get the Balloon with a concrete value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Instance of one certain balloon</returns>
         public IBalloon GetBalloon(int value)
         {
             if (this.balloonsList.ContainsKey(value))
